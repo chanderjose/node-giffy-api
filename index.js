2e9aa254f2b81001de7f5105ee636e9eaa79f78c
Alexander Quevedo <chanderjose@gmail.com>
2021-05-31 23:22:55 -0400

    Remove deprecated 'body-parser' middleware

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 var cors = require('cors')
-const bodyParser = require('body-parser');
 
 const app = express()
 
 app.set('secretKey', '<<<SUPER-SECRET-KEY>>>');
 
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 // Define application endpoints
 var apiRoutes = require('./apiRoutes');
